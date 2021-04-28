9049c3aa4182434cfc927c8755b3e0f4b140f0e5
David Muller <davehmuller@gmail.com>
2021-04-27 18:04:52 -0700

    Update code to latest beta release - fix typo in variable_dne

--- code/variable_does_not_exist.py
+++ code/variable_does_not_exist.py
@@ -2,7 +2,7 @@ a = 1
 
 # `a + oops` will never work:
 # Traceback (most recent call last):
-#   File "variable_does_not_exist.py", line 7, in <module>
+#   File "variable_does_not_exist.py", line 8, in <module>
 #     a + oops
 # NameError: name 'oops' is not defined
 a + oops
